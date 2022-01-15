a0156ebf913e5d276da9d22e993a46786532ec5d
Irvin <adan.ordonez@plastinova.hn>
2022-01-15 11:58:23 -0600

    Conectandome a la tabla clientes para consultar los clientes agregados en la tabla.

---
+++ COBRANZAS/COBRANZAS/AdminDB/TParamSQL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace COBRANZAS.AdminDB
+{
+    public class TParamSQL
+    {
+        private SqlConnectionStringBuilder objBuilder = new SqlConnectionStringBuilder();
+
+        public TParamSQL()
+        {
+            objBuilder.DataSource = "IRVIN\\SQLEXPRESS";
+            objBuilder.InitialCatalog = "PROYECTO_CSHARP";
+            objBuilder.UserID = "sa";
+            objBuilder.Password = "itempo%123";
+        }
+
+        // Retorna la cadena de conexion a la base de datos.
+        public string  getStringCon()
+        {
+            return objBuilder.ConnectionString;
+        }
+    }
+}
